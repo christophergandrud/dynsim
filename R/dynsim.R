adf48008add89d579857401f13c0f0e6570e519f
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-09-14 15:58:28 +0200

    grunfeld example data added.

--- R/dynsim.R
+++ R/dynsim.R
@@ -3,11 +3,14 @@
 #' \code{dynsim} dynamic simulations of autoregressive relationships
 #' 
 #' @param ldv character. Names the lagged dependent variable
-#' @parm scen1 vector. Specifies the values of the variables used to generate the predicted values when \eqn{t = 0}.
+#' @param scen1 vector. Specifies the values of the variables used to generate the predicted values when \eqn{t = 0}.
 #' @param scen24 THINK ABOUT
+#' @param n numeric. Specifies the number of iterations (or time period) over which the program will generate the predicted value of the dependent variable. The default is 10.
 #' @param sig numeric. Specifies the level of statistical significance of the confidence intervals.
-#' 
-#' @param modify character vector. The name of up to four variables that modify the relationship between the shock vari- able and the dependent variable. 
+#'
+#' @param shock character. Allows the user to choose an independent variable (and its first \code{n} values) and have the variable (and potentially different values) impact the scenarios at each simulation. The number of values assigned to the shock variable must exceed the number of simulations. If this command is specified, the user must specify the \code{n} shock values with \code{shock_num}. If the shock variable is interacted with another variable in the model, the user must also specify the name of the modifying variable (\code{modify}) and the interaction variable (\code{inter}).
+#' @param shock_num numeric vector. Specify the shock values Any numeric vector is acceptable, as long as it contains at least \code{n} values.
+#' @param modify character vector. The name of up to four variables that modify the relationship between the shock variable and the dependent variable. 
 #' @param inter character vector. The name of up to four interaction variables. If \code{modify} is specified, this must also be specified.
 #' 
 #' 
@@ -20,7 +23,7 @@
 #' 
 #' @export
 
-dynsim <- function(ldv, scen1, scen24, modify = NULL, inter = NULL){
+dynsim <- function(ldv, scen1, scen2, scen3, scen4, n = 10, shock, shock_num, modify = NULL, inter = NULL){
 	# Make sure both modify and inter are given, if necessary.
 	if (!is.null(modify) & is.null(inter)){
 		stop("You must also specify the inter argument if you specify modify.")
@@ -32,4 +35,10 @@ dynsim <- function(ldv, scen1, scen24, modify = NULL, inter = NULL){
 	if (length(modify) != length(inter)){
 		stop("The number of modify variables must be the same as the number of inter variables.")
 	}	
+	# Error if number of iterations is <= 0.
+	if (n <= 0){
+		stop("You must specify at least 1 iteration with the n argument.")
+	}
+	# Get the number of scenarios specified by the user
+
 }
