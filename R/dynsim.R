6bc230136cf28958f4826faec1bf80e6fa7a2e96
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-09-15 12:06:40 +0200

    first working basic version

--- R/dynsim.R
+++ R/dynsim.R
@@ -4,10 +4,10 @@
 #' 
 #' @obj the output object from \code{\link{zelig}}.
 #' @param ldv character. Names the lagged dependent variable
-#' @param scen1 vector. Specifies the values of the variables used to generate the predicted values when \eqn{t = 0}.
+#' @param scen vector. Specifies the values of the variables used to generate the predicted values when \eqn{t = 0}.
 #' @param scen24 THINK ABOUT
 #' @param n numeric. Specifies the number of iterations (or time period) over which the program will generate the predicted value of the dependent variable. The default is 10.
-#' @param sig numeric. Specifies the level of statistical significance of the confidence intervals.
+#' @param sig numeric. Specifies the level of statistical significance of the confidence intervals. Any value allowed be greater than 0 and cannot be greater than 1.
 #'
 #' @param shock character. Allows the user to choose an independent variable (and its first \code{n} values) and have the variable (and potentially different values) impact the scenarios at each simulation. The number of values assigned to the shock variable must exceed the number of simulations. If this command is specified, the user must specify the \code{n} shock values with \code{shock_num}. If the shock variable is interacted with another variable in the model, the user must also specify the name of the modifying variable (\code{modify}) and the interaction variable (\code{inter}).
 #' @param shock_num numeric vector. Specify the shock values Any numeric vector is acceptable, as long as it contains at least \code{n} values.
@@ -24,10 +24,11 @@
 #' 
 #' @importFrom Zelig setx
 #' @importFrom Zelig sim
+#' @importFrom Zelig simulation.matrix
 #'
 #' @export
 
-dynsim <- function(obj, ldv, scen1, scen2, scen3, scen4, n = 10, shock, shock_num, modify = NULL, inter = NULL){
+dynsim <- function(obj, ldv, scen, n = 10, sig = 0.95, shock, shock_num, modify = NULL, inter = NULL){
 	# Make sure both modify and inter are given, if necessary.
 	if (!is.null(modify) & is.null(inter)){
 		stop("You must also specify the inter argument if you specify modify.")
@@ -43,18 +44,36 @@ dynsim <- function(obj, ldv, scen1, scen2, scen3, scen4, n = 10, shock, shock_nu
 	if (n <= 0){
 		stop("You must specify at least 1 iteration with the n argument.")
 	}
+	# Make sure sig is between 0 and 1.
+	if (sig <= 0 | sig > 1){
+		stop("sig must be greater than 0 and not greater than 1.")
+	}
+	# Create lower and upper bounds of the confidence interval
+	Bottom <- (1 - sig)/2
+	Top <- 1 - Bottom	
+
+	# Create data frame to fill in with simulation summaries
+	SimSum <- data.frame()
 
 	# Predicted values at time t0
-	
+	for (i in 1:n){
+		# Run simulations
+		SetVales <- setx(obj = obj, data = scen)
+		SimValues <- sim(obj = obj, x = SetVales)
 
-	
+		# Create summary data frame
+		PV <- simulation.matrix(SimValues, "Predicted Values: Y|X")
+		time <- i
+		ldvMean <- mean(PV)
+		ldvLower <- quantile(PV, prob = Bottom)
+		ldvUpper <- quantile(PV, prob = Top)
 
+		TempOut <- cbind(time, ldvMean, ldvLower, ldvUpper)
+		SimSum <- rbind(SimSum, TempOut)
 
-
-
-
-
-
+		# Change lag variable for the next simulation
+		scen[, ldv] <- ldvMean		
+	}
 
 
 
